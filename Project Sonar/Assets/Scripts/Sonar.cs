e229af52b5570f7792b0436e9ddd31364be4b576
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-01-20 19:56:41 +0000

    Day 1 End.
    Setup project and folders.
    
    Setup all currently needed prefabs and scripts.
    
    Sonar sprite created.
    
    Michael did lots of work on bat that looks way better than mine and
    ryan's shitty bat. ty micahel.

---
+++ Project Sonar/Assets/Scripts/Sonar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour
+{
+    //Default sonar scale is 0.08f
+    public GameObject levelSonar; //Reveals the level objects.
+    [SerializeField] private float sonarLifespan = 3f;
+    [SerializeField] private float scalingIncrement = 0.01f;
+
+    void Start()
+    {
+        ScaleSonar();
+    }
+
+    void ScaleSonar()
+    {
+        //Increase sonar size by scalingIncrement until sonarLifespan has passed. Maybe need to use IEnumerator.
+    }
+}
